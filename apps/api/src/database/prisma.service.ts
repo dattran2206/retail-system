fe8b6f189245070ae935829d5a41528edfc84e75
Trần <camtu181124@gmail.com>
2026-04-12 16:05:06 +0700

    remove unnecessary whitespace and add placeholder comment in PrismaService

--- apps/api/src/database/prisma.service.ts
+++ apps/api/src/database/prisma.service.ts
@@ -18,7 +18,7 @@ export class PrismaService extends PrismaClient implements OnModuleInit, OnModul
       ],
     });
   }
-
+////////////////rheryretyre
   async onModuleInit(): Promise<void> {
     try {
       await this.$connect();
